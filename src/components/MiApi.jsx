e045b69b616ddc14445f11eb887b9bb386b249c4
ssalinascalibar <ssalinascalibar@gmail.com>
2022-09-06 17:39:10 -0400

    first commit

---
+++ src/components/MiApi.jsx
@@ -0,0 +1,126 @@
+import React from 'react'
+import { useState, useEffect } from "react";
+import Modal from './Modal';
+
+const MiApi = () => {
+
+    // 3. info guardará los valores traídos desde la API 
+    const [buscarPersonajes, setBuscarPersonajes] = useState('');
+   
+    
+    const [sortAz, setSortAz] = useState('');
+    
+    
+    const [personajes, setPersonajes] = useState([]);
+    const [hechizos, setHechizos] = useState([]);
+
+    // 2. LLamamos al función que consume la API al momento de montar el componente
+    useEffect(() => { 
+        consultarInformacion();
+        consultarInformacionHechizos();
+
+    }, []);
+
+    // 1. Función que consulta la API
+    const consultarInformacion = async () => {
+        const url = 'https://fedeperin-harry-potter-api.herokuapp.com/personajes'; 
+        const response = await fetch(url)
+        const data = await response.json()
+
+        console.log(data)
+
+        setPersonajes([...personajes,...data]);
+        
+        // setInfo(`${data[1].personaje}`); 
+        // // con setInfo actualizamos el estado
+        
+    }
+
+    const consultarInformacionHechizos = async () => {
+        const url = 'https://fedeperin-harry-potter-api.herokuapp.com/hechizos'; 
+        const response = await fetch(url)
+        const data = await response.json()
+        
+        // data.forEach(d => {
+        //     d['hechizos_iso'] = ''
+            
+        // })
+
+
+        console.log(data)
+        setHechizos([...hechizos,...data]);
+        
+        // setInfo(`${data[1].personaje}`); 
+        // // con setInfo actualizamos el estado
+        
+    }
+
+    const filtrarPorPersonaje = (e) => {
+        setBuscarPersonajes(e.target.value);
+        console.log(buscarPersonajes);
+    }
+    
+
+  return (
+    <div className='content'>
+        <header>
+            <h1>Harry Potter</h1>
+            <form >
+                <input onChange={filtrarPorPersonaje} value={buscarPersonajes} className="form-control" type="text" placeholder="Busca un personaje"/>
+                <button onClick={() => setSortAz('1')} >Ordenar de la A a Z</button>
+                <button onClick={() => setSortAz('0')} >Ordenar de la Z a A</button>
+                {/* <select onChange={(e) => setSortAz(e.target.value)} value={sortAz} className="form-select" aria-label="Default select example">
+                    <option selected>Open this select menu</option>
+                    <option value={sortAz}>Ordena de A-Z</option>
+                    <option value=''>Ordena de </option>
+                    
+                </select> */}
+            </form>
+        </header>
+        <div className='row gx-4'>
+            {personajes.filter((e) => {
+                if(buscarPersonajes === '') {
+                    return e;
+                } else if (e.apodo.toLocaleLowerCase().startsWith(buscarPersonajes.toLocaleLowerCase())
+                ) {
+                    return e;
+                }
+                }).sort((e, e2) => (console.log(sortAz) === '1')?(e.apodo < e2.apodo):(e.apodo > e2.apodo))
+                
+                
+                
+                // .sort((e, e2) => (e.apodo > e2.apodo) ? 1 : (e.apodo < e2.apodo) ? -1 : 0)
+                
+                .map(p =>
+                <div className='col-12 col-md-6 col-lg-3 text-white mb-4' key={p.personaje}>
+                    <div className='card' >
+                        <div className='card-header'><h5>{p.personaje}</h5></div>    
+                        <img src={p.imagen} className="card-img-top" alt="..."></img>
+                        <div className='card-body'>
+                            <div className='card-title'><h2>{p.apodo}</h2></div>    
+                        </div>
+                        <ul className='list-group list-group-flush'>
+                            <li className='list-group-item list-group-item-dark'>Casa : {p.casaDeHogwarts}</li>
+                        </ul>
+                    </div>
+                </div>    
+            )
+            }
+        </div>
+        
+        <section className='hechizos'>
+            <div className='row gx-4'>
+                <h2>Hechizos</h2>
+                {hechizos.map(h =>
+                    <div className='col-4 col-md-6 col-lg-4 text-white mb-2' key={h.id}>
+                        <Modal hechizo={h.hechizo} uso={h.uso}/>
+                    </div>
+                )
+                }
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default MiApi
---
+++ src/components/Modal.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Modal = ({hechizo, uso}) => {
+  return (
+    <div>
+        
+        <div className="divModal" data-bs-toggle="modal" data-bs-target={'#' + hechizo}>
+        {hechizo}
+        </div>
+
+        {/* Modal */}
+        <div className="modal fade" id={hechizo} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+            <div className="modal-content openedModal">
+            <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">{hechizo}</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+                "{uso}"
+            </div>
+            <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                {/* <button type="button" className="btn btn-primary">Save changes</button> */}
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Modal
