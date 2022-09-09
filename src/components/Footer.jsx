662aae5bfbc44b07a4ed4a45b7c1131ab4a673cf
ssalinascalibar <ssalinascalibar@gmail.com>
2022-09-09 16:39:33 -0400

    add roboto font to header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
+      <div className="copyright">
         <p>Sebastian Salinas C. </p>
-        <p>&copy; 2022</p> 
+        <p>&copy; 2022</p>
+      </div>   
     </footer>
   )
 }
