6d88bc4c8be6b101e5d79cfbf45c09cb98eec074
ssalinascalibar <ssalinascalibar@gmail.com>
2022-09-08 20:56:11 -0400

    add footer with style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import MiApi from './components/MiApi';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <MiApi />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>Sebastian Salinas C. </p>
+        <p>&copy; 2022</p> 
+    </footer>
+  )
+}
+
+export default Footer
