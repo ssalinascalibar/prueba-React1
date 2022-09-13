debf4ed49b2fec044ac7ac7e7c8dacce5acd9c6d
ssalinascalibar <ssalinascalibar@gmail.com>
2022-09-13 13:44:59 -0300

    add rel=noreferrer to href

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <footer>
       <div className='oficial-site'>
-        <a href="https://www.wizardingworld.com" target="_blank">Ir al sitio oficial</a>
+        <a href="https://www.wizardingworld.com" rel="noreferrer" target="_blank">Ir al sitio oficial</a>
       </div>
       <div className="insta">
-        <i className="fa-brands fa-instagram"> <a href="https://www.instagram.com/harrypotterfilm/?hl=en" target="_blank"> Harry Potter</a></i>
+        <i className="fa-brands fa-instagram"> <a href="https://www.instagram.com/harrypotterfilm/?hl=en" rel="noreferrer" target="_blank"> Harry Potter</a></i>
       </div>
       <div className="copyright">
         <p>&copy; 2022 Sebastian Salinas C. Todos los derechos reservados </p>
